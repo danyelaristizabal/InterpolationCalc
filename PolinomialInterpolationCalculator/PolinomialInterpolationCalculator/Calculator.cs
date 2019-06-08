2d938d424316afba79d4e6ff41e1229adad9cb43
Danyel Aristizabal <danyelaristizabal@hotmail.com>
2019-06-09 02:26:08 +0300

    simple visual interface

--- PolinomialInterpolationCalculator/PolinomialInterpolationCalculator/Calculator.cs
+++ PolinomialInterpolationCalculator/PolinomialInterpolationCalculator/Calculator.cs
@@ -10,11 +10,21 @@ using System.Windows.Forms;
 
 namespace PolinomialInterpolationCalculator
 {
-    public partial class Form1 : Form
+    public partial class Main : Form
     {
-        public Form1()
+        public Main()
         {
             InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
